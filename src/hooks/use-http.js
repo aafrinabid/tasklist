f05141966663032e3cbe1e27e0ecd3ea0de9da50
aafrinabid <mohdaafrin@gmail.com>
2022-04-25 12:37:08 +0530

    kola mass

---
+++ src/hooks/use-http.js
@@ -0,0 +1,41 @@
+import { useState } from "react/cjs/react.production.min"; 
+
+
+
+const useHttp=(requestConfig,accesData)=>{
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const sendRequest = async (taskText) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          requestConfig.url,{
+            method:requestConfig.method?requestConfig.method:'GET',
+            headers:requestConfig.headers?requestConfig.headers:{},
+            body:request.config.body?JSON.stringify(requestConfig.body):null,  
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+  
+        const data = await response.json();
+        accesData(data)
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
+      }
+      setIsLoading(false);
+    };
+    return {
+        isLoading,
+        error,
+        sendRequest
+    }
+
+}
+
+export default useHttp
